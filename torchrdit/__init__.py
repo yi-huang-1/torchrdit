2b870a4ba435d9addb796609ce38cb4699ed4be4
yi-huang-1 <fecee.h@outlook.com>
2025-02-02 15:01:03 -0500

    feat(solver.py, cell.py, viz.py): added new features

--- torchrdit/__init__.py
+++ torchrdit/__init__.py
@@ -8,4 +8,12 @@ from . import logger
 from . import layers
 from . import viz
 
+<<<<<<< Updated upstream
 __version__ = "0.1.7"
+=======
+<<<<<<< Updated upstream
+__version__ = "0.1.6"
+=======
+__version__ = "0.1.8"
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
